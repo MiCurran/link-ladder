44e8f209d34f5425dbfda47a5492dc18ded17894
Micurran <mcurran1722@gmail.com>
2022-06-27 21:45:34 -0600

    add onboarding step structure

--- pages/auth/onboarding/new-user.js
+++ pages/auth/onboarding/new-user.js
@@ -1,39 +1,48 @@
 import { getSession } from 'next-auth/react';
-import UserView from '../../../views/User/UserView';
 import { supabase } from '../../../lib/supabase';
-import { Heading, Input, Text, Center, Box, Avatar,Button } from '@chakra-ui/react';
+import { useColorModeValue, Heading, Input, Text, Center, Box, Avatar,Button } from '@chakra-ui/react';
 import { useSession } from 'next-auth/react';
 import { useState } from 'react';
+import Stepper from '../../../components/Stepper/Stepper';
+import StepOne from '../../../views/Onboarding/Steps/StepOne';
+import StepTwo from '../../../views/Onboarding/Steps/StepTwo';
+import StepThree from '../../../views/Onboarding/Steps/StepThree';
+import StepFour from '../../../views/Onboarding/Steps/StepFour';
+
 const NewUser = ({newUser}) => {
     const { data: session } = useSession()
     const [userName, setUserName] = useState('');
+    const [currentStep, setCurrentStep] = useState(0);
+    
+    const steps = [
+        {
+            label: 'User Name',
+            component: <StepOne userName={userName} setUserName={setUserName} setCurrentStep={setCurrentStep}/>
+        },
+        {
+            label: 'User Name',
+            component: <StepTwo setCurrentStep={setCurrentStep} />,
+        },
+        {
+            label: 'User Name',
+            component: <StepThree setCurrentStep={setCurrentStep} />,
+        },
+        {
+            label: 'User Name',
+            component: <StepFour setCurrentStep={setCurrentStep} />,
+        },
+    ]
+    
 
     return (
-        <Center flexDir={'column'} h='100vh' py={20} justifyContent={'space-between'}>
-            <Heading>Let's Pick A User Name!</Heading>
-            <Avatar
-                size={'xl'}
-                src={session?.user?.image}
-            />
-            <Input
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                size={'lg'} 
-                placeholder='user name'
-            />
-                <Button
-                    disabled={!userName.length > 0}
-                    style={{opacity: !!userName.length ? 'initial' : '0'}}
-                >
-                    Next Step
-                </Button>
-            <Box>
-            <Text>
-                Link Ladder User Names Are Important! Your Micro landing page will 
-                live at linkladder.me/YOUR-USERNAME-WILL-BE-HERE
-            </Text>
-            <Text>So make it a good one!</Text>
-            </Box>
+        <Center 
+            flexDir={'column'} 
+            minH='100vh' 
+            py={20} 
+            justifyContent={'space-between'}
+        >
+            <Stepper steps={steps} setCurrentStep={setCurrentStep} />
+            {steps[currentStep].component}            
         </Center>
 );
 }
@@ -43,7 +52,6 @@ export default NewUser
 export async function getServerSideProps(context) {
     const session = await getSession(context);
     let newUser = null;
-    console.log("🚀 ~ file: new-user.js ~ line 17 ~ getServerSideProps ~ session", session)
     if (!session) {
         return {
             redirect: {
